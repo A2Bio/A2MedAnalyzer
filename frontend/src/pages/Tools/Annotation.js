706283703070c66d2ab8381fdb1d7963fcb3d94e
GnomGenome <kolesnikovaam2003@gmail.com>
2025-05-16 20:49:16 +0300

    19:55 Added buttons in visualization page

--- frontend/src/pages/Tools/Annotation.js
+++ frontend/src/pages/Tools/Annotation.js
@@ -3,7 +3,7 @@ import { QuestionCircleOutlined, SyncOutlined } from '@ant-design/icons';
 import { FloatButton } from 'antd';
 const Annotation = () => (
   <>
-    <FloatButton.Group shape="circle" style={{ insetInlineEnd: 24 }}>
+    <FloatButton.Group shape="circle">
       <FloatButton icon={<QuestionCircleOutlined />} />
       <FloatButton />
       <FloatButton.BackTop visibilityHeight={0} />
